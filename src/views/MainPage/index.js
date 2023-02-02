bebf3a141a15149e0a8dd30d69dbd08f65f9aec0
unknown <eefee1987@gmail.com>
2023-02-02 20:54:02 +0100

    resolucion de conflictos

--- src/views/MainPage/index.js
+++ src/views/MainPage/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { Header } from "../../Components/Header";
-import {NotesSection} from '../../Components/NotesSection'
+import { NotesSection } from "../../Components/NotesSection";
 
 export const MainPage = () => {
   return (
@@ -9,13 +9,8 @@ export const MainPage = () => {
         <button>Login</button>
         <button>Register</button>
       </Header>
-<<<<<<< HEAD
       <main>Contenido principal</main>
       <footer>Pie de pagina</footer>
-=======
-      <NotesSection title='Forget to remembre your chores, from now we take care of it'></NotesSection>
->>>>>>> c9df35402b4e4b72a8b94bc0aa95e3cc24fe6804
     </div>
   );
 };
-
