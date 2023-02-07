6ac947c2062bfd2b5ae6b5e4c786c6a655b5e81f
unknown <eefee1987@gmail.com>
2023-02-07 07:46:54 +0100

    añadido ping en notas

--- src/views/Register/index.js
+++ src/views/Register/index.js
@@ -8,42 +8,48 @@ import { Form } from "../../Components/Form";
 import { Header } from "../../Components/Header";
 import { Footer } from "../../Components/Footer";
 
-
 export const Register = () => {
   const navigate = useNavigate();
-  
+
   const [registerData, setregisterData] = useState({
-    email: '',
-    password: '',
-  })
+    email: "",
+    password: "",
+  });
 
   const handleInputChangeRegister = (e) => {
-    let {name, value} = e.target
-    if(name !== "repetir email") {
-    let newData = {...registerData, [name]:value}
-    setregisterData(newData)}
-  }
+    let { name, value } = e.target;
+    if (name !== "repetir email") {
+      let newData = { ...registerData, [name]: value };
+      setregisterData(newData);
+    }
+  };
 
   const handleSubmitRegister = async (e) => {
-    e.preventDefault()
-      try {
-       await axios.post("http://localhost:8000/users" , registerData)
-      swal("Registro exitoso", "por favor revisa la casilla de correo para activar tu cuenta", 'success')
-      navigate(`/login`)
-       } catch (error) {
-       swal("An error has occured",error.response.data.message, "error")
-      } 
-
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8000/users", registerData);
+      swal(
+        "Registro exitoso",
+        "por favor revisa la casilla de correo para activar tu cuenta",
+        "success"
+      );
+      navigate(`/login`);
+    } catch (error) {
+      swal("An error has occured", error.response.data.message, "error");
     }
+  };
   return (
     <div className="formRegister">
       <Header tituloVista="Register"></Header>
-      <Form handleInputChangeFunction = {handleInputChangeRegister} handleSubmitFunction = {handleSubmitRegister}
-      textoBoton="Register">
-       <div className="extra-input">
+      <Form
+        handleInputChangeFunction={handleInputChangeRegister}
+        handleSubmitFunction={handleSubmitRegister}
+        textoBoton="Register"
+      >
+        <div className="extra-input">
           <label htmlFor="repetiremail"> Repetir Contraseña</label>
-          <input  type="password" id="repetiremail" name="repetiremail" ></input>
-       </div>
+          <input type="password" id="repetiremail" name="repetiremail"></input>
+        </div>
       </Form>
       <Footer></Footer>
     </div>
